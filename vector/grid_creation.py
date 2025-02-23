5967152d126d3400208b21103afa193422fe29be
Nischal <nischal@Nischals-MacBook-Air.local>
2025-02-23 21:09:38 +0530

    updated the code for grid_creation

--- vector/grid_creation.py
+++ vector/grid_creation.py
@@ -5,37 +5,39 @@ import numpy as np
 import shapely
 import os
 
-india_boundary = gpd.read_file(os.path.join("Administrative Boundary Database", "india_boundary.shp")).to_crs(epsg = 4326)
 
-xmin, ymin, xmax, ymax = india_boundary.total_bounds
+def create_grid(input_path, output_path, grid_size):
 
-stepx, stepy = 10/111, 10/111 #This is create a grid approx 10x10km
+    boundary = gpd.read_file(input_path)
+    if boundary.crs.to_epsg() != 3857:
+        boundary = boundary.to_crs(epsg = 3857)
 
-grid_cells = []
+    xmin, ymin, xmax, ymax = boundary.total_bounds
 
-grids_no = -1
+    grid_cells = []
+    for x0 in np.arange(xmin, xmax, grid_size):
+        for y0 in np.arange(ymin, ymax, grid_size):
+            x1, y1 = x0+grid_size, y0+grid_size
+            new_cell = shapely.geometry.box(x0, y0, x1, y1)
 
-for x0 in np.arange(xmin, xmax+stepx, stepx):
-    for y0 in np.arange(ymin, ymax+stepy, stepy):
+            if new_cell.intersects(boundary['geometry'].any()):
+                grid_cells.append(new_cell)
 
-        x1 = x0+stepx
-        y1 = y0+stepy
-        new_cell = shapely.geometry.box(x0, y0, x1, y1)
+    grid_cells = gpd.GeoDataFrame(geometry=grid_cells, crs = boundary.crs)
+    grid_cells['grid_no'] = range(len(grid_cells))
 
-        if new_cell.intersects(india_boundary['geometry'].any()):
-            grids_no += 1
-            grid_cells.append(new_cell)
-        else:
-            pass
-grid_cells = gpd.GeoDataFrame(grid_cells, columns=['geometry'])
-grid_cells.to_file(os.path.join('grid_india', 'grid_10km.shp'), driver='ESRI Shapefile')
+    grid_cells.to_file(output_path, driver='ESRI Shapefile')
 
 
 if __name__ == '__main__':
-    fig, ax = plt.subplots()
-    india_boundary.plot(ax = ax)
-    india_boundary.plot(ax = ax, color = 'red')
-    grid_cells.plot(ax = ax, edgecolor='blue', facecolor = 'none')
-    print(len(grid_cells))
-    grid_cells.plot(ax = ax, edgecolor='black', facecolor = 'none')
-    plt.show()
+    # fig, ax = plt.subplots()
+    # # boundary.plot(ax = ax)
+    # boundary.plot(ax = ax, color = 'red')
+    # grid_cells.plot(ax = ax, edgecolor='blue', facecolor = 'none')
+    # print(len(grid_cells))
+    # # grid_cells.plot(ax = ax, edgecolor='black', facecolor = 'none')
+    # plt.show()
+
+    create_grid("/Users/nischal/projects/rasvec/Nagla_Dhanua/mathura_village_boundary.shp",
+                "/Users/nischal/projects/rasvec/grid_india/grid_mathura.shp", 500)
+    pass
