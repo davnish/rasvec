18d22997839ca8401e9ede93cd913ccd9371ed7d
Nischal <nischal@Nischals-MacBook-Air.local>
2025-03-06 18:03:35 +0530

    [add] files

--- setup.py
+++ setup.py
@@ -5,20 +5,21 @@ with open("README.md", "r") as f:
 
 setup(
     name="rasvec",
-    version="0.0.1",
+    version="0.0.8",
     description="A python library to ease the of the handling geospatial data.",
     author="Nischal Singh",
     author_email="nischal.singh38@gmail.com",
     url="https://github.com/davnish/rasvec.git",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    package_dir={"", "rasvec"},
+    package_dir={"": "rasvec"},
     packages=find_packages(where="rasvec"),
     install_requires=[
-        "geopandas >= 1.0.1",
-        "rasterio >= 1.3.10",
-        "patchify >= 0.2.3"
+        "geopandas>=0.8.0",
+        "rasterio>=1.3.10",
+        "patchify>=0.2.3"
         ],
-    python_requires = ">=3.12.9",
+    setup_reqires=['wheel'],
+    python_requires=">=3.9",
     license="MIT",
 )
