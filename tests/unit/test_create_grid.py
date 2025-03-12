d77147204e7607d66f8ad797b78a1bcda901929a
Nischal <nischal@Nischals-MacBook-Air.local>
2025-03-12 16:22:38 +0530

    [add] test

---
+++ tests/unit/test_create_grid.py
@@ -0,0 +1,13 @@
+import os
+import tempfile
+from pathlib import Path
+from rasvec import create_grid_on_vector
+
+def test_create_grid_on_vector():
+    base_dir = Path(__file__).parent.parent.parent
+    vector_file_path = os.path.join(base_dir, "examples", "sample_data", "vector", "vector.shp")
+    with tempfile.TemporaryDirectory() as temp_dir:
+        output_file_path = os.path.join(temp_dir, "grid.shp")
+        grid_cells = create_grid_on_vector(vector_file_path, 1000, output_file_path)
+        assert len(grid_cells) == 81 and os.path.exists(output_file_path)
+    
