01ffd1cf31fcf1917d5c9a019ec55edf6d0e1ac7
davnish <nischal.singh38@gmail.com>
2025-05-23 01:10:35 +0530

    add: dockerfile

--- tests/unit/test_create_grid.py
+++ tests/unit/test_create_grid.py
@@ -1,13 +1,13 @@
 import os
 import tempfile
 from pathlib import Path
-from rasvec import create_grid_on_vector
+from rasvec import grid_vector
 
-def test_create_grid_on_vector():
+def test_grid_vector():
     base_dir = Path(__file__).parent.parent.parent
     vector_file_path = os.path.join(base_dir, "docs", "examples", "sample_data", "vector", "vec", "vec.shp")
     with tempfile.TemporaryDirectory() as temp_dir:
         output_file_path = os.path.join(temp_dir, "grid.shp")
-        grid_cells = create_grid_on_vector(vector_file_path, 1000, output_file_path)
+        grid_cells = grid_vector(vector_file_path, 1000, output_file_path)
         assert len(grid_cells) == 81 and os.path.exists(output_file_path)
     
--- tests/unit/test_tms_to_geotiff.py
+++ tests/unit/test_tms_to_geotiff.py
@@ -7,5 +7,5 @@ def test_tms_to_geotiff():
     with tempfile.TemporaryDirectory() as temp_dir:
         temp_name = Path(temp_dir, 'output.tiff')
         bbox = [74.347916, 24.287027, 74.355469, 24.293128999999997]
-        tms_to_geotiff(output=temp_name, bbox=bbox, zoom=19, source='satellite', overwrite=True, quiet=True)
+        tms_to_geotiff(output=temp_name.as_posix(), bbox=bbox, zoom=19, source='satellite', overwrite=True, quiet=True)
         assert temp_name.exists()
